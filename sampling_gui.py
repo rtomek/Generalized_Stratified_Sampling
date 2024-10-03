4d8bc23cb95e0dd17437b2444d16ed288ea1b021
rtomek <rtomek@uchicago.edu>
2024-10-03 13:44:09 -0500

    This should clean up the GUI a bit

--- sampling_gui.py
+++ sampling_gui.py
@@ -41,33 +41,40 @@ class SamplingApp(QWidget):
         form_layout.addRow(QLabel("Filename:"), filename_layout)
 
         self.dataset_column_input = QLineEdit('dataset')
+        self.dataset_column_input.setToolTip("The name of the column in the data file that contains the dataset names.")
         form_layout.addRow(QLabel("Dataset Column:"), self.dataset_column_input)
 
         self.features_input = QLineEdit('sex,age_at_index,race,ethnicity,covid19_positive')
+        self.features_input.setToolTip("The features (columns) to use for the calculation. The features should be comma-separated.")
         form_layout.addRow(QLabel("Features (comma-separated):"), self.features_input)
 
         # self.title_input = QLineEdit()
         # form_layout.addRow(QLabel("Title:"), self.title_input)
 
-        self.datasets_input = QLineEdit("{\"Open\": 0.8, \"Seq\": 0.2}")
-        form_layout.addRow(QLabel("Datasets (JSON format, e.g., {\"Fold 1\": 20, \"Fold 2\": 20, \"Fold 3\": 20, \"Fold 4\": 20, \"Fold 5\": 20}):"), self.datasets_input)
+        self.datasets_input = QLineEdit("{\"Fold 1\": 20, \"Fold 2\": 20, \"Fold 3\": 20, \"Fold 4\": 20, \"Fold 5\": 20}")
+        self.datasets_input.setToolTip("The datasets to use for the calculation. The keys are the dataset names and the values are the fractions of the data to use for each dataset.")
+        form_layout.addRow(QLabel("Datasets (JSON format, e.g., {\"Train\": 0.8, \"Validation\": 0.2}):"), self.datasets_input)
 
         self.numeric_cols_input = QLineEdit("{\"age_at_index\": {\"bins\": [0, 10, 20, 30, 40, 50, 60, 70, 80, 89, 100], \"labels\": None}}")
+        self.numeric_cols_input.setToolTip("The numeric columns to use for the calculation. The keys are the column names and the values are dictionaries containing the bins and labels for the column.")
         form_layout.addRow(QLabel("Numeric Columns (JSON format, e.g., {\"age_at_index\": {\"bins\": [0, 10, 20, 30, 40, 50, 60, 70, 80, 89, 100], \"labels\": None}, \"col2\": {\"bins\": None, \"labels\": None}}):"), self.numeric_cols_input)
 
         self.uid_col_input = QLineEdit("submitter_id")
+        self.uid_col_input.setToolTip("The name of the column in the data file that contains the unique identifier for each row.")
         form_layout.addRow(QLabel("Unique Identifier Column:"), self.uid_col_input)
 
         self.layout.addLayout(form_layout)
 
         self.load_button = QPushButton("Load File and Perform Sampling")
+        self.load_button.setToolTip("Load the data file from the disk and perform the sampling.")
         self.load_button.clicked.connect(self.load_file_and_sample)
         self.layout.addWidget(self.load_button)
 
         self.table_view = QTableView()
         self.layout.addWidget(self.table_view)
 
-        self.save_button = QPushButton("Save Output to CSV")
+        self.save_button = QPushButton("Save Output to CSV/TSV")
+        self.save_button.setToolTip("Save the output to a CSV/TSV file.")
         self.save_button.clicked.connect(self.save_output)
         self.layout.addWidget(self.save_button)
 
@@ -76,7 +83,10 @@ class SamplingApp(QWidget):
         self.sampled_df = None
 
     def browse_file(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "CSV Files (*.csv);;Excel Files (*.xlsx, *.xls);;TSV Files (*.tsv)")
+        file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Supported Files (*.csv *.tsv *.xlsx *.xls);;"
+                                                                          "TSV Files (*.tsv);;"
+                                                                          "CSV Files (*.csv);;"
+                                                                          "Excel Files (*.xlsx *.xls)")
         if file_path:
             self.filename_input.setText(file_path)
 
@@ -112,18 +122,25 @@ class SamplingApp(QWidget):
 
             # Load the CSV or Excel file
             file_path = filename
-            # file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "CSV Files (*.csv);;Excel Files (*.xlsx)")
             if not file_path:
                 return
 
-            if file_path.endswith('.csv'):
-                df = pd.read_csv(file_path)
-            elif file_path.endswith('.xlsx') or file_path.endswith('.xls'):
-                df = pd.read_excel(file_path)
-            elif file_path.endswith('.tsv'):
-                df = pd.read_csv(file_path, sep='\t')
+            # Map file extensions to corresponding pandas read functions
+            read_functions = {
+                '.xlsx': pd.read_excel,
+                '.xls': pd.read_excel,
+                '.csv': pd.read_csv,
+                '.tsv': lambda file: pd.read_csv(file, sep='\t')
+            }
+
+            # Get the file extension
+            file_ext = filename[filename.rfind('.'):]
+
+            # Check if the extension is supported and read the file
+            if file_ext in read_functions:
+                df = read_functions[file_ext](filename)
             else:
-                QMessageBox.critical(self, "Invalid File", "Please select a valid CSV or Excel file.")
+                QMessageBox.critical(self, "Invalid File", "Please select a valid TSV, CSV or Excel file.")
                 return
 
             df = midrc_clean(df, sampling_data)
@@ -159,13 +176,26 @@ class SamplingApp(QWidget):
 
     def save_output(self):
         if self.sampled_df is not None:
-            file_path, _ = QFileDialog.getSaveFileName(self, "Save File", "", "TSV Files (*.tsv);;CSV Files (*.csv)")
-            if file_path:
-                if file_path.endswith('.tsv'):
-                    self.sampled_df.to_csv(file_path, index=False, sep='\t')
-                elif file_path.endswith('.csv'):
-                    self.sampled_df.to_csv(file_path, index=False)
+            file_path, _ = QFileDialog.getSaveFileName(self, "Save File", "", "TSV Files (*.tsv);;"
+                                                                           "CSV Files (*.csv);;"
+                                                                           "Excel Files (*.xlsx *.xls)")
+            # Map file extensions to corresponding pandas write functions
+            write_functions = {
+                '.xlsx': lambda file: self.sampled_df.to_excel(file, index=False),
+                '.xls': lambda file: self.sampled_df.to_excel(file, index=False),
+                '.csv': lambda file: self.sampled_df.to_csv(file, index=False),
+                '.tsv': lambda file: self.sampled_df.to_csv(file, index=False, sep='\t')
+            }
+
+            # Get the file extension
+            file_ext = file_path[file_path.rfind('.'):]
+
+            # Check if the extension is supported and save the file
+            if file_ext in write_functions:
+                write_functions[file_ext](file_path)
                 QMessageBox.information(self, "File Saved", "File has been saved successfully.")
+            else:
+                QMessageBox.critical(self, "Invalid File Extension", "Please select a valid file extension.")
         else:
             QMessageBox.warning(self, "No Data", "There is no data to save.")
 
